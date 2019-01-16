54ebeeb1a6d9230111bb5ee870062a484a24d9ab
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2019-01-16 13:21:11 +0100

    Add header to include gl.h on multiple platforms

---
+++ src/gl/includes_gl.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of psylib.
+ *
+ * psylib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * psylib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psylib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file includes platform specific OpenGL libraries.
+ */
+
+#include <psy_config.h>
+
+#if defined(HAVE_GL_H)
+    #include <GL/gl.h>
+#endif
+
+#ifdef HAVE_APPLE_GL_H
+    #include <OpenGL/gl.h>
+#endif
