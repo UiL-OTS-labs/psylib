be978af368aeb9dedaa445727ae3460ca414384e
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2019-01-16 17:47:39 +0100

    Opengl add shader for OpenGL

--- src/Shader.c
+++ src/Shader.c
@@ -14,3 +14,203 @@
  * You should have received a copy of the GNU General Public License
  * along with psylib.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
+#include <assert.h>
+#include <DynamicArray.h>
+#include "MetaClass.h"
+#include "Shader.h"
+#include "Error.h"
+#include "gl/GLError.h"
+
+/* **** functions that implement PsyShader or override SeeObject **** */
+
+static int
+shader_init(
+    PsyShader*              shader,
+    const PsyShaderClass*   shader_cls,
+    psy_shader_t            type
+    )
+{
+    int ret = SEE_SUCCESS;
+    const SeeObjectClass* parent_cls = SEE_OBJECT_CLASS(
+        shader_cls
+        );
+        
+    parent_cls->object_init(SEE_OBJECT(shader), parent_cls);
+
+    shader->shader_id   = 0;
+    shader->shader_type = type;
+    
+    return ret;
+}
+
+static int
+init(const SeeObjectClass* cls, SeeObject* obj, va_list args)
+{
+    const PsyShaderClass* shader_cls = PSY_SHADER_CLASS(cls);
+    PsyShader* shader = PSY_SHADER(obj);
+    
+    /*Extract parameters here from va_list args here.*/
+
+    psy_shader_t type = va_arg(args, psy_shader_t);
+
+    return shader_cls->shader_init(
+        shader,
+        shader_cls,
+        type
+        );
+}
+
+static int
+shader_id(const PsyShader* shader, GLuint* id)
+{
+    const PsyShaderClass* cls = PSY_SHADER_GET_CLASS(shader);
+
+    if (!shader)
+        return SEE_INVALID_ARGUMENT;
+
+    return cls->id(shader, id);
+}
+
+static int
+shader_type(const PsyShader* shader, psy_shader_t* type)
+{
+    const PsyShaderClass* cls = PSY_SHADER_GET_CLASS(shader);
+
+    if (!shader)
+        return SEE_INVALID_ARGUMENT;
+
+    return cls->type(shader, type);
+}
+
+static int
+shader_compile(PsyShader* shader, const char* src, SeeError** error)
+{
+    int success;
+    SeeError* err = NULL;
+    char log[BUFSIZ] = {0};
+
+    glShaderSource(shader->shader_id, 1, &src, NULL);
+    glCompileShader(shader->shader_id);
+
+    /* Check whether compilation succeeded. */
+    glGetShaderiv(shader->shader_id, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader->shader_id, sizeof(log), NULL, log);
+        int status = psy_glerror_create((PsyGLError**)(&err));
+        assert(status == SEE_SUCCESS);
+        psy_error_printf(PSY_ERROR(err), "Unable to compile shader:\n%s", log);
+        *error = err;
+        return SEE_RUNTIME_ERROR;
+    }
+
+    return SEE_SUCCESS;
+}
+
+static int
+shader_compile_file(PsyShader* shader, FILE* file, SeeError** error)
+{
+    int c, ret;
+    char character;
+    char null_byte = '0';
+    const char* string = NULL;
+    const PsyShaderClass* cls = PSY_SHADER_GET_CLASS(shader);
+
+    SeeDynamicArray* array = NULL;
+
+    ret = see_dynamic_array_new_capacity(
+        &array,
+        sizeof(char),
+        NULL,
+        NULL,
+        NULL,
+        1024
+        );
+
+    if (ret != SEE_SUCCESS)
+        return ret;
+
+    while ((c = fgetc(file)) != EOF) {
+        character = (char) c;
+        ret = see_dynamic_array_add(array, &character);
+        if (ret != SEE_SUCCESS) {
+            goto shader_compile_file_error;
+        }
+    }
+    ret = see_dynamic_array_add(array, &null_byte);
+    if (ret)
+        goto shader_compile_file_error;
+
+    if (ferror(file)) {
+        ret = SEE_RUNTIME_ERROR;
+        goto shader_compile_file_error;
+    }
+
+    string = see_dynamic_array_get(array, 0);
+    ret = cls->shader_compile(shader, string, error);
+
+    //cleanup and exit
+    shader_compile_file_error:
+    see_object_decref(SEE_OBJECT(array));
+    return ret;
+}
+
+/* **** implementation of the public API **** */
+
+/* **** initialization of the class **** */
+
+PsyShaderClass* g_PsyShaderClass = NULL;
+
+static int psy_shader_class_init(SeeObjectClass* new_cls) {
+    int ret = SEE_SUCCESS;
+    
+    /* Override the functions on the parent here */
+    new_cls->init = init;
+
+    /* Set the function pointers of the own class here */
+    PsyShaderClass* cls = (PsyShaderClass*) new_cls;
+
+    cls->shader_init         = shader_init;
+    cls->type                = shader_type;
+    cls->id                  = shader_id;
+    cls->shader_compile      = shader_compile;
+    cls->shader_compile_file = shader_compile_file;
+    
+    return ret;
+}
+
+/**
+ * \private
+ * \brief this class initializes PsyShader(Class).
+ *
+ * You might want to call this from the library initialization func.
+ */
+int
+psy_shader_init() {
+    int ret;
+    const SeeMetaClass* meta = see_meta_class_class();
+
+    ret = see_meta_class_new_class(
+        meta,
+        (SeeObjectClass**) &g_PsyShaderClass,
+        sizeof(PsyShaderClass),
+        sizeof(PsyShader),
+        see_object_class(),
+        sizeof(SeeObjectClass),
+        psy_shader_class_init
+        );
+
+    return ret;
+}
+
+void
+psy_shader_deinit()
+{
+    if(!g_PsyShaderClass)
+        return;
+
+    see_object_decref((SeeObject*) g_PsyShaderClass);
+    g_PsyShaderClass = NULL;
+}
+
--- src/Shader.h
+++ src/Shader.h
@@ -16,15 +16,133 @@
  */
 
 
-#ifndef Shader_H
-#define Shader_H
+#ifndef PSY_SHADER_H
+#define PSY_SHADER_H
+
+#include <Error.h>
+#include <stdio.h>
+#include "SeeObject.h"
+#include "psy_export.h"
+#include "gl/includes_gl.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef struct _PsyShader PsyShader;
+typedef struct _PsyShaderClass PsyShaderClass;
+
+typedef enum _psy_shader_t {
+    PSY_SHADER_VERTEX,
+    PSY_SHADER_FRAGMENT
+    // Other shaders are currently not supported.
+} psy_shader_t;
+
+struct _PsyShader {
+    SeeObject parent_obj;
+    /*expand PsyShader data here*/
+    psy_shader_t    shader_type;
+    GLuint          shader_id;
+};
+
+struct _PsyShaderClass {
+    SeeObjectClass parent_cls;
+    
+    int (*shader_init)(
+        PsyShader*            shader,
+        const PsyShaderClass* shader_cls,
+        psy_shader_t          type
+        );
+
+    /* expand PsyShader class with extra functions here.*/
+
+    int (*type) (const PsyShader* shader, psy_shader_t* out);
+
+    int (*id)   (const PsyShader* shader, GLuint* out);
+
+    int (*shader_compile)     ( PsyShader* shader,
+                                const char* src,
+                                SeeError** error
+                                );
+
+    int (*shader_compile_file)( PsyShader* shader,
+                                FILE*      file,
+                                SeeError** error
+                                );
+};
+
+/* **** function style macro casts **** */
+
+/**
+ * \brief cast a pointer from a PsyShader derived instance back to a
+ *        pointer to PsyShader.
+ */
+#define PSY_SHADER(obj)                      \
+    ((PsyShader*) obj)
+
+/**
+ * \brief cast a pointer to PsyShaderClass derived class back to a
+ *        pointer to PsyShaderClass.
+ */
+#define PSY_SHADER_CLASS(cls)                      \
+    ((const PsyShaderClass*) cls)
+
+/**
+ * \brief obtain a pointer to PsyShaderClass from a instance of
+ *        derived from PsyShader. This macro is preferably
+ *        used when obtaining the class of a instance. When this
+ *        macro is used. Calling methods on the class will enable
+ *        polymorphism, because you'll get the derived class.
+ */
+#define PSY_SHADER_GET_CLASS(obj)                \
+    (PSY_SHADER_CLASS(see_object_get_class(SEE_OBJECT(obj)) )  )
+
+
+/* **** public functions **** */
+
+/**
+ * @brief Create a new vertex or fragment shader.
+ *
+ * Creates a new updated shader.
+ *
+ * @param [out]     shader  A pointer to a pointer to PsyShader
+ * @param [in]      type    PSY_VERTEX_SHADER or PSY_FRAGMENT_SHADER
+ * @param [out]     error   If an error occurs it might be returned at error.
+ *                          if it does the error should be decreffed.
+ * @return
+ */
+PSY_EXPORT int
+psy_shader_create(PsyShader** shader, psy_shader_t type, SeeError** error);
+
+
+/**
+ * Gets the pointer to the PsyShaderClass table.
+ */
+PSY_EXPORT const PsyShaderClass*
+psy_shader_class();
+
+/* Expand the class with public functions here, don't forget the SEE_EXPORT
+ * macro, because otherwise you'll run into troubles when exporting function
+ * in a windows dll.
+ */
+
+/* **** class initialization functions **** */
+
+/**
+ * Initialize PsyShader; make it ready for use.
+ */
+PSY_EXPORT
+int psy_shader_init();
+
+/**
+ * Deinitialize PsyShader, after PsyShader has been deinitialized,
+ * all functions in this header shouldn't be used anymore.
+ */
+PSY_EXPORT
+void psy_shader_deinit();
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif //ifndef Shader_H
+#endif //ifndef PSY_SHADER_H
